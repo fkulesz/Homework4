6189c3914a5d6e9c48899e4b467ac193bd72db09
fkulesz <fkulesz@poczta.onet.pl>
2020-01-30 12:46:51 +0100

    zad4 Device commit 4

---
+++ src/task4/Device.java
@@ -0,0 +1,30 @@
+package task4;
+
+public class Device {
+
+    private String code;
+    private String producer;
+    private String type;
+    private double price;
+
+    Device(String code, String producer, String type, double price){
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+        this.price = price;
+    }
+    Device(String code, String producer, String type){
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+
+    public String getInfo() {
+        return "Kod : " +
+                 code + '\'' +
+                ", producent: " + producer + '\'' +
+                ", typ: " + type + '\'' +
+                ", cena: " + price ;
+
+    }
+}
